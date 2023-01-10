ace6d38f4d0f60fee1e5f20a5bfa84887b48cf21
varsha Gunjal <varshashingote01@gmail.com>
2023-01-10 10:06:26 +0530

    [varsha] Adding HelloWrld Project

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello varsha welcome .net ");
+          
+
+            Console.WriteLine("welcome to new journy...");
+            Console.WriteLine("hello varsha in git hub");
+            Console.ReadLine();
+            
+        }
+    }
+}
