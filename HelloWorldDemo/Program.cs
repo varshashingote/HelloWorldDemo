b7389780ab73667d61aae41f2637d97b9c2df8bd
varsha Gunjal <varshashingote01@gmail.com>
2023-01-11 09:25:43 +0530

    modified

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -15,9 +15,10 @@ namespace HelloWorldDemo
 
             Console.WriteLine("welcome to new journy...");
             Console.WriteLine("hello varsha in git hub");
+               Console.WriteLine("this is local line");
+            Console.WriteLine("This is remote line");
 
             Console.WriteLine("this is local line");
-            Console.WriteLine("This is remote line");
             Console.ReadLine();
             
         }
